8d51f934aa2c2fea41a70334a2ccc0d3a8642124
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2023-04-01 11:24:48 +0200

    feat: add actors and information

--- src/InfoWave/Attribute.cs
+++
@@ -1,7 +0,0 @@
-namespace InfoWave;
-
-/// <summary>
-/// Represents a specific attribute such as color, shape, etc.
-/// </summary>
-/// <param name="Id">Unique name of the attribute.</param>
-public record Attribute(string Id) : Identifier("attribute", Id);
--- src/InfoWave/AttributeValue.cs
+++
@@ -1,8 +0,0 @@
-namespace InfoWave;
-
-/// <summary>
-/// Represents a value given to a specific attribute.
-/// </summary>
-/// <param name="Attribute">Attribute the value corresponds to.</param>
-/// <param name="Value">The value of the specified attribute.</param>
-public record AttributeValue(Attribute Attribute, string Value);
